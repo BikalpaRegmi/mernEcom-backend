30dde3fe4d349c04ba9aae44b92bf0443d1799ff
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2023-12-10 12:17:16 +0545

    make it better

--- Router/userRoute.js
+++ Router/userRoute.js
@@ -58,7 +58,10 @@ router.route('/signIn').post(async (req, res) => {
 
             res.cookie('ecommerceCookie' , token , {
                expires:new Date(Date.now() + 999999999),
-               httpOnly: true,                 
+               httpOnly: true,    
+               sameSite: 'None',
+               domain: 'https://ecommerceappmern.onrender.com/', 
+
             })
 
             return res.json({ msg: 'Sign-in successful' });
