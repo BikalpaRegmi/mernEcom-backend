427c1b8234b51d2f184b35f22b7a7b689344d103
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2023-12-11 14:15:38 +0545

    b

--- Router/userRoute.js
+++ Router/userRoute.js
@@ -59,6 +59,7 @@ router.route('/signIn').post(async (req, res) => {
             res.cookie('ecommerceCookie' , token , {
                expires:new Date(Date.now() + 999999999),
                httpOnly:true,
+               domain: '.netlify.app',
             })
 
             return res.json({ msg: 'Sign-in successful' });
